266c99312f5f8a2f50f999a84075e789f233ce85
阎嘉晖 <100705236+studentyanjiahui@users.noreply.github.com>
2023-08-17 21:21:01 +0800

    Add files via upload

---
+++ 前端/dist/assets/index-1c5e6985.js
@@ -0,0 +1,1 @@
+import{U as r}from"./index-8c98e848.js";const i={to:[String,Object],url:String,replace:Boolean};function s({to:e,url:o,replace:t,$router:n}){e&&n?n[t?"replace":"push"](e):o&&(t?location.replace(o):location.href=o)}function a(){const e=r().proxy;return()=>s(e)}export{i as r,a as u};
